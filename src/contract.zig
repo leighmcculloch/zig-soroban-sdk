508943a6c1d6099e9219bce5d769c7c53d0f1cfd
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-08-26 21:00:17 +1000

    wip

--- src/contract.zig
+++ src/contract.zig
@@ -1,5 +1,4 @@
-const val = @import("val.zig");
-const Val = val.Val;
+const Val = @import("val.zig").Val;
 
 const MyType = extern struct {
     address: Val,
@@ -10,9 +9,14 @@ export fn add(ia: Val, ib: Val, ic: Val) Val {
     const a = ia.toBool();
     const b = ib.toU32();
     const c = ic.toU32();
+    const r = _add(a, b, c);
+    return Val.fromU32(r);
+}
+
+fn _add(a: bool, b: u32, c: u32) u32 {
     if (a) {
-        return Val.fromU32(b + c);
+        return b + c;
     } else {
-        return Val.fromU32(0);
+        return 0;
     }
 }
